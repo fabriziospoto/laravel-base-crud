4c99670809d6ce0463326526b522e50a963ff401
Fabrizio <fabriziospoto1@gmail.com>
2020-10-15 16:39:58 +0200

    index
    index

---
+++ database/migrations/2020_10_15_142429_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('personaggio',50);
+            $table->string('storia',100);
+            $table->integer('numero');
+            $table->string('editore',50);
+            $table->integer('anno');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
